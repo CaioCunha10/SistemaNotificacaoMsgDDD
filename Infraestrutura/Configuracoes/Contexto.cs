d8124409036c0ac5d4c4d83bed361561d5b44de0
Nathalia Maia <nathaliamaia@MacBook-Air-de-Nathalia.local>
2024-03-26 01:58:21 -0300

    ArquivoConfig BD Entity to migrations

--- Infraestrutura/Configuracoes/Contexto.cs
+++ Infraestrutura/Configuracoes/Contexto.cs
@@ -10,5 +10,34 @@ namespace Infraestrutura.Configuracoes
 {
     public class Contexto : IdentityDbContext<ApplicationUser>
     {
+        public Contexto (DbContextOptions<Contexto> opcoes) : base(opcoes)
+        {  
+        }
+
+        public DbSet<Noticia> Noticia {get; set;}
+
+        public dbSet <ApplicationUser> ApplicationUser  { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+         {
+            if(!optionsBuilder.IsConfigured)
+               {
+                 optionsBuilder.UseSqlServer(ObterStringConexao());
+                 base.OnConfiguring(optionsBuilder);
+               }
+         }
+         protected override void OnModelCreating(Modelbuilder builder)
+         {
+            builder.Entity<ApplicationUser>().ToTable("AspNetUser").HasKey(t=> t.Id);
+            base.OnModelCreating(builder);
+         }
+           
+        public string ObterStringConexao()
+        {
+            //stringconnection here
+            string strcon = ""
+            return strcon;
+        }
     }
+    
 }
